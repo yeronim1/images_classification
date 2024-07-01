a8463910d447de5b59f01e02fa3b459bc89131ea
Yaroslav Morgun <morgunaroslav@gmail.com>
2024-07-01 09:30:31 +0300

    fix datagen

--- conf_matrix.py
+++ conf_matrix.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -14,14 +15,15 @@ classes = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
 # Шлях до тестової директорії
 test_dir = './seg_test'
 
+test_datagen = ImageDataGenerator(rescale=1./255)
+
 # Генератор зображень для тест сету
-test_generator = tf.keras.preprocessing.image.ImageDataGenerator().flow_from_directory(
+test_generator = test_datagen.flow_from_directory(
     test_dir,
     target_size=(150, 150),
     batch_size=32,
     class_mode='categorical',
-    shuffle=False,
-    rescale=1.0/255
+    shuffle=False
 )
 # Предікт для тест сету
 Y_pred = model.predict(test_generator)
